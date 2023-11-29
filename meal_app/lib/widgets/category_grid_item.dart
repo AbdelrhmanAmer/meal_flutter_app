62087aacf5ab031bc456b7fe77b85fefb4526a87
AbdelrhmanAmer <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>
2023-11-29 14:27:34 +0200

    Navigator

--- meal_app/lib/widgets/category_grid_item.dart
+++ meal_app/lib/widgets/category_grid_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:meal_app/screens/meals_screen.dart';
 
 import '../models/category.dart';
 
@@ -10,13 +11,19 @@ class CategoryGridItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return InkWell( // GestureDetector
-      onTap: (){},
+      onTap: (){
+        Navigator.of(context).push(
+          MaterialPageRoute(
+              builder: (ctx)=>const MealsScreen(),
+          ),
+        );
+      },
       splashColor: Theme.of(context).primaryColor,
       borderRadius: BorderRadius.circular(16),
       child: Container(
           padding: const EdgeInsets.all(16),
           decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10),
+              borderRadius: BorderRadius.circular(4),
               gradient: LinearGradient(
                 colors: [
                   category.color.withOpacity(0.54),
