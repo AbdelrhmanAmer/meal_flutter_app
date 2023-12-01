9bbfd213be8f5c0b493af52ac55ba86ca82bcae1
AbdelrhmanAmer <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>
2023-12-01 07:38:37 +0200

    Positioned

--- meal_app/lib/widgets/meal_item.dart
+++ meal_app/lib/widgets/meal_item.dart
@@ -13,6 +13,10 @@ class MealItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
+      margin: const EdgeInsets.all(8),
+      shape: RoundedRectangleBorder( borderRadius: BorderRadius.circular(15) ),
+      elevation: 10,
+      clipBehavior: Clip.hardEdge,
       child: InkWell(
         child: Stack(
           children: [
@@ -21,7 +25,34 @@ class MealItem extends StatelessWidget {
                 placeholder: MemoryImage(kTransparentImage),
                 image: NetworkImage(meal.imageUrl),
             ),
-            Text(meal.title, style: const TextStyle(color: Colors.white),),
+            Positioned(
+              bottom: 0,
+              right: 0,
+              left: 0,
+              child: Container(
+                padding: const EdgeInsets.symmetric(vertical: 6, horizontal: 44),
+                color: Colors.black54,
+                child: Column(
+                  children: [
+                    Text(
+                      meal.title,
+                      textAlign: TextAlign.center,
+                      maxLines: 2,
+                      softWrap: true,
+                      overflow: TextOverflow.ellipsis, // word...
+                      style: const TextStyle(
+                          color: Colors.white,
+                          fontSize: 20
+                      ),
+                    ),
+                    const SizedBox(height: 12,),
+                    const Row(
+                      children: [ ],
+                    )
+                  ],
+                ),
+              ),
+            ),
           ],
         ),
       ),
