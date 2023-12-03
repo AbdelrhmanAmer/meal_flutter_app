4b124496724e1bb03d033b86f3198f0e7ead49af
AbdelrhmanAmer <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>
2023-12-03 14:04:55 +0200

    NavBar Provider

--- meal_app/lib/screens/tabs_screen.dart
+++ meal_app/lib/screens/tabs_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:meal_app/providers/navBar_provider.dart';
 import '../models/meal.dart';
 import '../providers/favorites_provider.dart';
 import '../providers/filters_provider.dart';
@@ -8,67 +9,46 @@ import '../screens/filter_screen.dart';
 import '../screens/meals_screen.dart';
 import '../widgets/main_drawer.dart';
 
-class TabsScreen extends ConsumerStatefulWidget {
+class TabsScreen extends ConsumerWidget {
   const TabsScreen({super.key});
 
   @override
-  ConsumerState<TabsScreen> createState() => _TabsScreenState();
-}
-
-class _TabsScreenState extends ConsumerState<TabsScreen> {
-  int _selectedPageIndex = 0;
-
-  void _setScreen(String identifier){
-    Navigator.pop(context);
-    if(identifier == "filters"){
-      Navigator.of(context).push(
-        MaterialPageRoute(builder: (ctx){
-          return const FiltersScreen();
-        })
-      );
-
-    }else if(identifier == "meals"){
-      Navigator.of(context).pop();
-    }
-  }
-
-  void _selectPage(int index){
-    setState(() {
-      _selectedPageIndex = index;
-    });
-  }
-
-
-  @override
-  Widget build(BuildContext context) {
-
+  Widget build(BuildContext context, WidgetRef ref) {
     final List<Meal> availableMeals = ref.watch(filteredMealsProvider);
+    final int selectedPageIndex = ref.watch(navBarProvider);
 
     Widget activePage =  CategoryScreen(
       availableMeals: availableMeals,
     );
 
     String activePageTitle = 'Pick your category';
-
-    if(_selectedPageIndex == 1){
+    if(selectedPageIndex == 1){
       final List<Meal> favoriteMeals = ref.watch(favoritesMealsProvider);
-      setState(() {
         activePage = MealsScreen(
             meals: favoriteMeals,
         );
         activePageTitle = 'Favorites';
-      });
     }
-
     return Scaffold(
       appBar: AppBar(
         title: Text(activePageTitle),
       ),
       body: activePage,
-      drawer:  MainDrawer(onSelectScreen: _setScreen,),
+      drawer:  MainDrawer(onSelectScreen: (String identifier){
+        Navigator.pop(context);
+        if(identifier == "filters"){
+          Navigator.of(context).push(
+              MaterialPageRoute(builder: (ctx){
+                return const FiltersScreen();
+              })
+          );
+        }else if(identifier == "meals"){
+          Navigator.of(context).pop();
+        }
+      } ),
       bottomNavigationBar: BottomNavigationBar(
-        onTap: _selectPage,
-        currentIndex: _selectedPageIndex,
+        onTap: ref.watch(navBarProvider.notifier).selectPage,
+        currentIndex: selectedPageIndex,
         items: const [
           BottomNavigationBarItem(
               icon: Icon(Icons.fastfood_outlined),
