ccd5227a27543c1e487115fe705590a7957ca37c
AbdelrhmanAmer <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>
2023-12-04 21:30:28 +0200

    Grid Animation

--- meal_app/lib/screens/categories_screen.dart
+++ meal_app/lib/screens/categories_screen.dart
@@ -4,29 +4,70 @@ import 'package:meal_app/widgets/category_grid_item.dart';
 
 import '../models/meal.dart';
 
-class CategoryScreen extends StatelessWidget {
+class CategoryScreen extends StatefulWidget {
   const CategoryScreen({
     super.key,
     required this.availableMeals
   });
   final List<Meal> availableMeals;
+
+  @override
+  State<CategoryScreen> createState() => _CategoryScreenState();
+}
+
+class _CategoryScreenState extends State<CategoryScreen> with SingleTickerProviderStateMixin{
+  late AnimationController _controller;
+  late Animation<Offset> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 400),
+      lowerBound: 0,
+      upperBound: 1,
+    )..forward();
+
+    _animation = Tween(
+        begin: const Offset(0, .3),
+        end: const Offset(0, 0)
+    ).animate(CurvedAnimation(
+      curve: Curves.linear,
+      parent: _controller,
+    ));
+  }
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    _controller.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return GridView(
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2,
-            mainAxisSpacing: 20,
-            crossAxisSpacing: 20,
-            childAspectRatio: 3/2, // width : height
+    return AnimatedBuilder(
+      animation: _controller,
+      builder: (context, child)=> SlideTransition(
+        position: _animation,
+        child: child,
+      ),
+      child: GridView(
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+              mainAxisSpacing: 20,
+              crossAxisSpacing: 20,
+              childAspectRatio: 3/2, // width : height
 
-          ),
-        children: [
-          for(final category in availableCategories)
-            CategoryGridItem(
-              category: category,
-              availableMeals: availableMeals,
             ),
-        ],
-      );
+          children: [
+            for(final category in availableCategories)
+              CategoryGridItem(
+                category: category,
+                availableMeals: widget.availableMeals,
+              ),
+          ],
+        ),
+    );
   }
 }
